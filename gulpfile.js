d50d95955d51a970243cb68ec46efc25b02ea801
Batonczyk <dxfanat@gmail.com>
2022-08-21 01:12:16 +0200

    page1

--- gulpfile.js
+++ gulpfile.js
@@ -11,6 +11,10 @@ const concat = require('gulp-concat');
 const autoprefixer = require('gulp-autoprefixer');
 const uglify = require('gulp-uglify');
 const imagemin = require('gulp-imagemin');
+const svgSprite = require('gulp-svg-sprite');
+const cheerio = require('gulp-cheerio');
+const replace = require('gulp-replace');
+const plumber = require('gulp-plumber');
 const del = require('del');
 const browserSync = require('browser-sync');
 
@@ -41,6 +45,7 @@ function styles() {
 function scripts() {
   return src([
       'node_modules/jquery/dist/jquery.js',
+      'node_modules/slick-carousel/slick/slick.js',
       'app/js/main.js'
     ])
     .pipe(concat('main.min.js'))
@@ -75,6 +80,32 @@ function images() {
     .pipe(dest('dist/images'))
 }
 
+
+function svgSprites() {
+  return src('app/images/icons/*.svg')
+    .pipe(plumber())
+    .pipe(cheerio({
+      run: ($) => {
+        $("[fill]").removeAttr("fill");
+        $("[stroke]").removeAttr("stroke");
+        $("[style]").removeAttr("style");
+      },
+      parserOptions: {
+        xmlMode: true
+      },
+    }))
+    .pipe(
+      svgSprite({
+        mode: {
+          stack: {
+            sprite: '../sprite.svg',
+          },
+        },
+      })
+    )
+    .pipe(dest('app/images'));
+}
+
 function build() {
   return src([
       'app/**/*.html',
@@ -94,6 +125,7 @@ function cleanDist() {
 function watching() {
   watch(['app/scss/**/*.scss'], styles);
   watch(['app/js/**/*,js', '!app/js/main.min.js'], scripts);
+  watch(['app/images/icons/*.svg'], svgSprites);
   watch(['app/**/*.html']).on('change', browserSync.reload);
 }
 
@@ -106,7 +138,8 @@ exports.browsersync = browsersync;
 exports.uglify = uglify;
 exports.watching = watching;
 exports.images = images;
+exports.svgSprites = svgSprites;
 exports.cleanDist = cleanDist;
 exports.build = series(cleanDist, images, build);
 
-exports.default = parallel(styles, scripts, browsersync, watching);
+exports.default = parallel(svgSprites, styles, scripts, browsersync, watching);

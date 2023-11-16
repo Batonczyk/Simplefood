3ed4269c047ac51195acfd2028abb7ee9dbcff8f
Batonczyk <dxfanat@gmail.com>
2023-11-16 21:20:05 +0100

    delete coments in code main.js

--- app/js/main.js
+++ app/js/main.js
@@ -1,37 +1,28 @@
 $(function () {
-
   window.onscroll = function showHeader() {
-
     var header = document.querySelector('.header__top');
-
     if (window.pageYOffset > 200) {
       header.classList.add('header__top--fixed');
     } else {
       header.classList.remove('header__top--fixed');
     }
-
   };
-
   var wow = new WOW({
-    boxClass: 'wow', // animated element css class (default is wow)
-    animateClass: 'animate__animated', // animation css class (default is animated)
-    offset: 0, // distance to the element when triggering the animation (default is 0)
-    mobile: true, // trigger animations on mobile devices (default is true)
-    live: true, // act on asynchronously loaded content (default is true)
+    boxClass: 'wow', 
+    animateClass: 'animate__animated', 
+    offset: 0,
+    mobile: true, 
+    live: true, 
     callback: function (box) {
-      // the callback is fired every time an animation is started
-      // the argument that is passed in is the DOM node being animated
     },
-    scrollContainer: null, // optional scroll container selector, otherwise use window,
-    resetAnimation: true, // reset animation on end (default is true)
+    scrollContainer: null, 
+    resetAnimation: true, 
   });
   wow.init();
-
 
   $('.menu-burger').on('click', function () {
     $('.burger-nav').toggleClass('burger-nav--active');
     $('.burger-overlay').toggleClass('burger-overlay--active');
-    // $('.body').toggleClass('lock');
   });
 
   $('.catalog-btn').on('click', function () {
@@ -46,7 +37,6 @@ $(function () {
   });
 
   $(".menu, .footer, .header-content").on("click", "a", function () {
-    // event.preventDefault();
     var id = $(this).attr('href'),
       top = $(id).offset().top;
     $('body,html').animate({
@@ -63,7 +53,6 @@ $(function () {
     $($(this).attr('href')).addClass('tabs-wrapper__content-item--active');
   });
 
-
   $(".product-item__star, .star, .reviews__star, .reviews-content__star").rateYo({
     rating: '4',
     starWidth: '17px',
@@ -72,14 +61,9 @@ $(function () {
     starSvg: String,
     starSvg: '<svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg"><g clip-path="url(#clip0_3197_359)"><path d="M0.022973 6.16432C0.0780978 5.9946 0.224753 5.87091 0.401315 5.84529L5.36139 5.12451L7.57966 0.629933C7.6586 0.469933 7.82157 0.368652 7.99997 0.368652C8.17841 0.368652 8.34135 0.469933 8.42032 0.629933L10.6387 5.12451L15.5987 5.84529C15.7752 5.87091 15.9219 5.9946 15.977 6.16429C16.0322 6.334 15.9862 6.52028 15.8584 6.64482L12.2694 10.1434L13.1165 15.0834C13.1467 15.2593 13.0744 15.437 12.9301 15.5419C12.8484 15.6012 12.7517 15.6314 12.6545 15.6314C12.5799 15.6314 12.505 15.6136 12.4365 15.5776L8 13.2451L3.56374 15.5775C3.40577 15.6606 3.21443 15.6467 3.07009 15.5419C2.92574 15.437 2.8534 15.2593 2.88356 15.0834L3.73096 10.1434L0.141567 6.64478C0.0138172 6.52028 -0.0322143 6.334 0.022973 6.16432Z"/></g><defs><clipPath id="clip0_3197_359"><rect width="16" height="16" fill="white" transform="matrix(-1 0 0 1 16 0)"/></clipPath></defs></svg>'
   });
-
   $('.select-style, .product-item__input').styler();
-
   var mixer = mixitup('.category-menu');
-
 });
-
-
 var $range = $(".catalog-price__input"),
   $inputFrom = $(".catalog-price__min"),
   $inputTo = $(".catalog-price__max"),
@@ -88,53 +72,41 @@ var $range = $(".catalog-price__input"),
   max = 1000,
   from = 100,
   to = 1000;
-
 $('.catalog-price__input').ionRangeSlider({
   type: "double",
   prefix: "₴",
   onStart: updateInputs,
   onChange: updateInputs
 });
-
 instance = $range.data("ionRangeSlider");
-
 function updateInputs(data) {
   from = data.from;
   to = data.to;
-
   $inputFrom.prop("value", from);
   $inputTo.prop("value", to);
 }
-
 $inputFrom.on("input", function () {
   var val = $(this).prop("value");
-
   if (val < min) {
     val = min;
   } else if (val > to) {
     val = to;
   }
-
   instance.update({
     from: val
   });
 });
-
 $inputTo.on("input", function () {
   var val = $(this).prop("value");
-
   if (val < from) {
     val = from;
   } else if (val > max) {
     val = max;
   }
-
   instance.update({
     to: val
   });
 });
-
-
 
 $('.interest-box__menu').slick({
   slidesToShow: 5,
@@ -277,5 +249,4 @@ const fancyboxTool = {
       "</button>",
   },
 };
-
 $('[data-fancybox="gallery"]').fancybox(fancyboxTool);
